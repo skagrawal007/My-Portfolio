c5544c271a9916f9a92eabccd7109c3fbee73b51
skagrawal007 <sk047094@gmail.com>
2024-08-11 10:45:42 +0530

    updated index file

--- scripts.js
+++ scripts.js
@@ -1,24 +1,33 @@
 let header = document.querySelector("header");
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
+
+// Add the dark mode class initially
+document.body.classList.add("active");
+
+// Change the icon to sun initially to reflect the dark mode state
+let darkmode = document.querySelector("#darkmode");
+darkmode.classList.replace("bx-moon", "bx-sun");
+
 window.addEventListener("scroll", () => {
     header.classList.toggle("shadow", window.scrollY > 0);
 });
+
 menu.onclick = () => {
     navbar.classList.toggle("active");
 };
+
 window.onscroll = () => {
     navbar.classList.remove("active");
 };
-// Dark Mode / light mode
-let darkmode = document.querySelector("#darkmode");
+
+// Dark Mode / Light Mode
 darkmode.onclick = () => {
     if (darkmode.classList.contains("bx-moon")) {
         darkmode.classList.replace("bx-moon", "bx-sun");
         document.body.classList.add("active");
-    }
-    else {
+    } else {
         darkmode.classList.replace("bx-sun", "bx-moon");
         document.body.classList.remove("active");
     }
-} 
+}
